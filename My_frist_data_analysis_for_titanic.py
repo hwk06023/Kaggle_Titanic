7d4890680261b6c7baaef12b3a529a468be6a27c
김현우 <hwk06023@gmail.com>
2019-07-30 07:34:52 +0900

    via upload

---
+++ My_frist_data_analysis_for_titanic.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('seaborn')
+sns.set(font_scale=2.5)
+
+import missingno as msno
+
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+%matplotlib inline
+
+
+df_train = pd.read_csv('../input/train.csv')
+df_test = pd.read_csv('../input.test.csv')
